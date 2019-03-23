43ffa142627fbe5ed6eeeafd9076cc667d980cb8
sergiusqa <sergius.la@gmail.com>
2019-03-22 22:05:08 -0700

    work notes

---
+++ func/exe_time.py
@@ -0,0 +1,6 @@
+# NOTE: Execution time measurement - https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
+import time
+
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))
